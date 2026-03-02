678830e524ad05f5ea0e316f2538a31d81b1bea8
developedbybarley@gmail.com <71340344+DevelopedByBarley@users.noreply.github.com>
2026-03-02 13:29:03 +0100

    index php bugfix

--- public/index.php
+++ public/index.php
@@ -8,9 +8,6 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 // Event types ['ftfl', 'artificial_intelligence', 'information_security', 'drone_technology']
-define('EVENT_TYPE', $_ENV['EVENT_TYPE']);   
-define('ABSTRACT_TYPE', $_ENV['ABSTRACT_TYPE']);
-define('EVENT_STARTED', $_ENV['EVENT_STARTED']);
 
 const BASE_PATH = __DIR__ . '/../';
 require_once __DIR__ . '/../vendor/autoload.php';
@@ -18,7 +15,12 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->safeLoad();
 
+define('EVENT_TYPE', $_ENV['EVENT_TYPE']);
+define('ABSTRACT_TYPE', $_ENV['ABSTRACT_TYPE']);
+define('EVENT_STARTED', $_ENV['EVENT_STARTED']);
+
 Session::create();
+
 
 require_once __DIR__ . '/../core/functions.php';
 require_once __DIR__ . '/../config/app.php';
